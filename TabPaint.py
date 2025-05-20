3c6f5e0ac166ee53da75ad87963fbe5410db6358
Guchiev Magomed-Bashir <magomedbashir095@gmail.com>
2025-05-20 21:50:58 +0300

    Add jeneral widget and add tabWidget

---
+++ TabPaint.py
@@ -0,0 +1,87 @@
+from PIL import Image
+from Paint import Paint
+from TabRow import TabRow
+from Consts import *
+
+
+class TabPaint(Paint):
+    def __init__(self, size=TABLATURES_ORIGINAL_SIZE,
+                 name_background_image=DEFAULT_NAME_TABLATURES_BACKGROUND_IMAGE,
+                 name_image=DEFAULT_NAME_TABLATURES_IMAGE, columns=None,
+                 tablatures_size=TABLATURES_SIZE, global_name="tablatures"):
+        super().__init__(size, name_background_image, name_image)
+        if columns is None:
+            columns = [TabRow() for _ in range(DEFAULT_NUMBER_OF_COLUMNS)]
+        self.columns = columns
+        self.tablatures_size = tablatures_size
+        self.global_name = global_name
+
+    def clear_one_position_finale_image(self, position,
+                                        name_finale_image=DEFAULT_NAME_FINALE_TABLATURES_IMAGE,
+                                        change_default_tablature=False):
+        if self.columns[position[0]].tab_row[position[1]] == -2:
+            return
+        self.clear_rectangle_finale_image(self.coordinates_by_position(position),
+                                          self.tablatures_size, name_finale_image)
+        if change_default_tablature:
+            self.columns[position[0]].tab_row[position[1]] = -2
+
+    def clear_all_tablatures(self, name_finale_image=DEFAULT_NAME_FINALE_TABLATURES_IMAGE,
+                             change_default_tablature=False):
+        for i in range(len(self.columns)):
+            for j in range(len(self.columns[i].tab_row)):
+                self.clear_one_position_finale_image((i, j), name_finale_image,
+                                                     change_default_tablature)
+
+    def change_one_tablatures(self, position, name_finale_image=DEFAULT_NAME_FINALE_TABLATURES_IMAGE):
+        self.clear_rectangle_finale_image(self.coordinates_by_position(position),
+                                          self.tablatures_size, name_finale_image)
+        self.draw_a_picture_by_number(self.columns[position[0]].tab_row[position[1]],
+                                      self.coordinates_by_position(position), self.tablatures_size,
+                                      name_finale_image)
+
+    def save(self, original_size=TABLATURES_ORIGINAL_SIZE):
+        name_tablatures = PATH_TABLATURES + self.global_name + ".png"
+        with Image.open(self.name_background_image) as background_image:
+            background_image.save(name_tablatures)
+        for i in range(len(self.columns)):
+            for j in range(len(self.columns[i].tab_row)):
+                self.change_one_tablatures((i, j), name_tablatures)
+
+    def set_global_name(self, new_global_name):
+        self.global_name = new_global_name
+
+    def set_cell_status(self, row_number, new_name):
+        self.columns[row_number].name = new_name
+
+    def update_tablatures_position(self, position, new_tablature,
+                                   name_finale_image=DEFAULT_NAME_FINALE_TABLATURES_IMAGE):
+        if len(new_tablature) == 0 or new_tablature == 'x' or new_tablature == '-' or new_tablature.isdigit() or (new_tablature[0] == '-' and new_tablature[1:].isdigit()):
+            if len(new_tablature) == 0 or new_tablature == '-':
+                self.columns[position[0]].tab_row[position[1]] = -2
+            elif new_tablature == 'x':
+                self.columns[position[0]].tab_row[position[1]] = -1
+            else:
+                if int(new_tablature) <= -2:
+                    self.columns[position[0]].tab_row[position[1]] = -2
+                elif int(new_tablature) >= 50:
+                    self.columns[position[0]].tab_row[position[1]] = 50
+                else:
+                    self.columns[position[0]].tab_row[position[1]] = int(new_tablature)
+            self.change_one_tablatures(position, name_finale_image)
+
+    def get_global_name(self):
+        return self.global_name
+
+    def get_position_name(self, position):
+        return self.columns[position].name
+
+    def get_cell_status(self, position):
+        if self.columns[position[0]].tab_row[position[1]] == -2:
+            return "-"
+        if self.columns[position[0]].tab_row[position[1]] == -1:
+            return "x"
+        return str(self.columns[position[0]].tab_row[position[1]])
+
+    def coordinates_by_position(self, position):
+        return 400 + position[0] * 380, 220 + position[1] * 120
---
+++ ТabWidget.py
@@ -0,0 +1,135 @@
+import sys
+
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene
+from PyQt5.QtGui import QPixmap
+from PyQt5 import uic
+
+from TabPaint import TabPaint
+from TabCellValidator import TabCellValidator
+
+from Consts import DEFAULT_NAME_FINALE_TABLATURES_IMAGE
+
+
+class TabWidget(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("src/tab_widget.ui", self)
+
+        self.tabs = TabPaint()
+        self.tabs.clear_all_finale_image(DEFAULT_NAME_FINALE_TABLATURES_IMAGE, False)
+
+        self.resetButton.clicked.connect(self.reset_pressed)
+        self.submitButton.clicked.connect(self.submit_pressed)
+
+        self.actual_row = 0
+
+        self.position_01.clicked.connect(lambda: self.change_actual_row(1))
+        self.position_02.clicked.connect(lambda: self.change_actual_row(2))
+        self.position_03.clicked.connect(lambda: self.change_actual_row(3))
+        self.position_04.clicked.connect(lambda: self.change_actual_row(4))
+        self.position_05.clicked.connect(lambda: self.change_actual_row(5))
+        self.position_06.clicked.connect(lambda: self.change_actual_row(6))
+        self.position_07.clicked.connect(lambda: self.change_actual_row(7))
+        self.position_08.clicked.connect(lambda: self.change_actual_row(8))
+        self.position_09.clicked.connect(lambda: self.change_actual_row(9))
+        self.position_10.clicked.connect(lambda: self.change_actual_row(10))
+        self.position_11.clicked.connect(lambda: self.change_actual_row(11))
+        self.position_12.clicked.connect(lambda: self.change_actual_row(12))
+        self.position_13.clicked.connect(lambda: self.change_actual_row(13))
+        self.position_14.clicked.connect(lambda: self.change_actual_row(14))
+        self.position_15.clicked.connect(lambda: self.change_actual_row(15))
+        self.position_16.clicked.connect(lambda: self.change_actual_row(16))
+
+        self.lineEditChord.textChanged.connect(self.tab_chord_changed)
+
+        self.lineEditString_1.textChanged.connect(lambda: self.string_status_changed(1))
+        self.lineEditString_2.textChanged.connect(lambda: self.string_status_changed(2))
+        self.lineEditString_3.textChanged.connect(lambda: self.string_status_changed(3))
+        self.lineEditString_4.textChanged.connect(lambda: self.string_status_changed(4))
+        self.lineEditString_5.textChanged.connect(lambda: self.string_status_changed(5))
+        self.lineEditString_6.textChanged.connect(lambda: self.string_status_changed(6))
+
+        self.lineEditString_1.setValidator(TabCellValidator())
+        self.lineEditString_2.setValidator(TabCellValidator())
+        self.lineEditString_3.setValidator(TabCellValidator())
+        self.lineEditString_4.setValidator(TabCellValidator())
+        self.lineEditString_5.setValidator(TabCellValidator())
+        self.lineEditString_6.setValidator(TabCellValidator())
+
+        self.lineEditName.textChanged.connect(self.tabs_name_changed)
+        self.refresh_global_name_input()
+
+    def update_tab_menu(self):
+        index = self.actual_row
+        self.lineEditString_1.textChanged.connect(lambda: self.tabs.get_cell_status((index, 0)))
+        self.lineEditString_2.textChanged.connect(lambda: self.tabs.get_cell_status((index, 1)))
+        self.lineEditString_3.textChanged.connect(lambda: self.tabs.get_cell_status((index, 2)))
+        self.lineEditString_4.textChanged.connect(lambda: self.tabs.get_cell_status((index, 3)))
+        self.lineEditString_5.textChanged.connect(lambda: self.tabs.get_cell_status((index, 4)))
+        self.lineEditString_6.textChanged.connect(lambda: self.tabs.get_cell_status((index, 5)))
+        self.lineEditChord.textChanged.connect(lambda: self.tabs.get_position_name(index))
+
+    def change_actual_row(self, index):
+        self.actual_row = index - 1
+        self.update_tab_menu()
+        self.refresh_string_inputs()
+        self.refresh_chord_input()
+
+    def refresh_chord_input(self):
+        name = self.tabs.get_position_name(self.actual_row)
+        self.lineEditChord.blockSignals(True)
+        self.lineEditChord.setText(str(name))
+        self.lineEditChord.blockSignals(False)
+
+    def refresh_string_inputs(self):
+        for string_idx in range(6):
+            val = self.tabs.get_cell_status((self.actual_row, string_idx))
+            le: QtWidgets.QLineEdit = getattr(self, f"lineEditString_{string_idx + 1}")
+            le.blockSignals(True)
+            le.setText(str(val))
+            le.blockSignals(False)
+
+    def refresh_global_name_input(self):
+        global_name = self.tabs.get_global_name()
+        self.lineEditName.blockSignals(True)
+        self.lineEditName.setText(str(global_name))
+        self.lineEditName.blockSignals(False)
+
+    def tab_chord_changed(self):
+        new_name = self.lineEditChord.text()
+        self.tabs.set_cell_status(self.actual_row, new_name)
+        self.update_visual_display()
+
+    def string_status_changed(self, index):
+        string = eval(f"self.lineEditString_{index}")
+        new_element = string.text()
+        self.tabs.update_tablatures_position((self.actual_row, index - 1), new_element)
+        self.update_visual_display()
+
+    def submit_pressed(self):
+        self.tabs.save()
+
+    def reset_pressed(self):
+        self.tabs.clear_all_tablatures(DEFAULT_NAME_FINALE_TABLATURES_IMAGE, True)
+        self.refresh_global_name_input()
+        self.update_tab_menu()
+        self.update_visual_display()
+
+    def tabs_name_changed(self, new_name):
+        self.tabs.set_global_name(new_name)
+
+    def update_visual_display(self):
+        scene = QGraphicsScene()
+        pixmap = QPixmap(DEFAULT_NAME_FINALE_TABLATURES_IMAGE)
+        scene.addPixmap(pixmap)
+        self.graphicsView.setScene(scene)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = TabWidget()
+    ex.show()
+    sys.exit(app.exec_())
